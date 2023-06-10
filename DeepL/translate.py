0b910fb0bcaf440f437d31582f5ebf0aee5018aa
風 緑 <mail@nekokawa.net>
2023-06-10 16:29:25 +0900

    Update translate.py

--- DeepL/translate.py
+++ DeepL/translate.py
@@ -3,43 +3,6 @@ import time
 from fake_useragent import UserAgent
 
 ua = UserAgent()
-testjson = {
-            "jsonrpc": "2.0",
-            "method": "LMT_handle_jobs",
-            "params": {
-                "jobs": [
-                    {
-                        "kind": "default",
-                        "sentences": [
-                            {
-                                "text": "This is a test.",
-                                "id": 0,
-                                "prefix": ""
-                            }
-                        ],
-                        "raw_en_context_before": [],
-                        "raw_en_context_after": [],
-                        "preferred_num_beams": 4,
-                        "quality": "fast"
-                    }
-                ],
-                "lang": {
-                    "user_preferred_langs": [
-                        "EN",
-                        "JA"
-                    ],
-                    "source_lang_user_selected": "EN",
-                    "target_lang": "JA"
-                },
-                "priority": -1,
-                "commonJobParams": {
-                    "mode": "translate",
-                    "browserType": 1
-                },
-                "timestamp": int(time.time())
-            },
-            "id": 46500010
-        }
 
 
 def translate(text, source_lang, target_lang):
